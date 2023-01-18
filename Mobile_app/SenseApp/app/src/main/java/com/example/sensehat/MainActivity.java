cd33a796b6a0d9000a8ef1820147f7c577d8bef9
MusePL <smodli99@gmail.com>
2023-01-19 00:44:49 +0100

    Added graph library

--- Mobile_app/SenseApp/app/src/main/java/com/example/sensehat/MainActivity.java
+++ Mobile_app/SenseApp/app/src/main/java/com/example/sensehat/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_leds, R.id.nav_charts)
+                R.id.nav_home, R.id.nav_leds, R.id.nav_charts, R.id.nav_data, R.id.nav_options)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
--- Mobile_app/SenseApp/app/src/main/java/com/example/sensehat/ui/charts/ChartsFragment.java
+++ Mobile_app/SenseApp/app/src/main/java/com/example/sensehat/ui/charts/ChartsFragment.java
@@ -1,5 +1,6 @@
 package com.example.sensehat.ui.charts;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,15 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.sensehat.databinding.FragmentChartsBinding;
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ChartsFragment extends Fragment {
 
@@ -24,8 +34,85 @@ public class ChartsFragment extends Fragment {
         binding = FragmentChartsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textCharts;
-        ChartsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        LineChart chart = (LineChart) binding.chart;
+
+        ArrayList<String> xAxisValues = new ArrayList<String>();
+        xAxisValues.add("Term1");
+        xAxisValues.add("Term2");
+        xAxisValues.add("Term3");
+        xAxisValues.add("Term4");
+        xAxisValues.add("Term5");
+        xAxisValues.add("Term6");
+
+        chart.getXAxis().setValueFormatter(new com.github.mikephil.charting.formatter.IndexAxisValueFormatter(xAxisValues));
+
+
+        ArrayList<Entry> entries = new ArrayList<>();
+
+        entries.add(new Entry(0, 60));//20    2018f  (2018f, 20))
+        entries.add(new Entry(1, 57));//40    2019f   (2019f, 40))
+        entries.add(new Entry(2, 65));//60    2020f (2020f, 60))
+        entries.add(new Entry(3, 70));//80    2021f (2021f, 80))
+        entries.add(new Entry(4, 80));//80    2021f (2021f, 80))
+        entries.add(new Entry(5, 70));//80    2021f (2021f, 80))
+
+
+        ArrayList<Entry> entry = new ArrayList<>();
+        entry.add(new Entry(0, 70));
+        entry.add(new Entry(1, 50));
+        entry.add(new Entry(2, 60));
+        entry.add(new Entry(3, 65));
+        entry.add(new Entry(4, 75));
+        entry.add(new Entry(5, 80));
+
+        ArrayList<Entry> marathi = new ArrayList<>();
+        marathi.add(new Entry(0, 80));
+        marathi.add(new Entry(1, 70));
+        marathi.add(new Entry(2, 50));
+        marathi.add(new Entry(3, 68));
+        marathi.add(new Entry(4, 55));
+        marathi.add(new Entry(5, 36));
+
+
+        ArrayList<LineDataSet> lines = new ArrayList<LineDataSet>();
+
+        LineDataSet set1 = new LineDataSet(entries, "English");
+        set1.setDrawFilled(true);
+        set1.setFillColor(Color.WHITE);
+        set1.setColor(Color.RED);
+        set1.setCircleColor(Color.DKGRAY);
+        lines.add(set1);
+
+        LineDataSet set2 = new LineDataSet(entry, "Hindi");
+        set2.setDrawFilled(true);
+        set2.setFillColor(Color.WHITE);
+        set2.setColor(Color.BLUE);
+        set2.setCircleColor(Color.RED);
+        lines.add(set2);
+
+        LineDataSet set3 = new LineDataSet(marathi, "Marathi");
+        set3.setDrawFilled(true);
+        set3.setFillColor(Color.WHITE);
+        set3.setColor(Color.YELLOW);
+        set3.setCircleColor(Color.parseColor("#8B0000"));
+        lines.add(set3);
+
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1);
+        dataSets.add(set2);
+        dataSets.add(set3);
+
+        chart.setData(new LineData(dataSets));
+
+        chart.getDescription().setText("");
+
+        chart.getDescription().setTextColor(Color.RED);
+
+
+        chart.animateY(1400, Easing.EaseInOutBounce);
+//        final TextView textView = binding.textCharts;
+//        ChartsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
