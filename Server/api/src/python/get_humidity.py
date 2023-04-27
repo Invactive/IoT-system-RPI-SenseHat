22e17925f603a3d140d51cd64bdcd71773711132
Jakub Grzesiak <gkuba8@gmail.com>
2023-04-27 14:41:22 +0200

    Update get_humidity.py

--- Server/api/src/python/get_humidity.py
+++ Server/api/src/python/get_humidity.py
@@ -2,27 +2,18 @@
 import json
 from sense_emu import SenseHat
 import time
-<<<<<<< HEAD
 import datetime
-=======
->>>>>>> Mobile_app
 
 
 sense = SenseHat()
 
 while(True):
     humi = sense.get_humidity()
-<<<<<<< HEAD
     date = {"timestamp": str(datetime.datetime.now())}
     data = json.dumps({"humidity": humi})
     data = json.loads(data)
     data.update(date)
     f = open("/home/jakub/Desktop/IoT-system-RPI-SenseHat/Server/api/src/data/humidity.dat", "w")
     f.write(json.dumps(data))
-=======
-
-    f = open("/home/jakub/Desktop/IoT-system-RPI-SenseHat/Server/api/src/data/humidity.dat", "w")
-    f.write(json.dumps({"humidity": humi}))
->>>>>>> Mobile_app
     f.close()
     time.sleep(0.1)
