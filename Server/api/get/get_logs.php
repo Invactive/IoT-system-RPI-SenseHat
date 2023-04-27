83e6da8a998337c93802c5b8e7f9d13d6d3b45a7
Jakub Grzesiak <gkuba8@gmail.com>
2023-04-27 14:34:52 +0200

    Update get_logs.php

--- Server/api/get/get_logs.php
+++ Server/api/get/get_logs.php
@@ -1,22 +1,8 @@
 <?php
-<<<<<<< HEAD
     $http_origin = $_SERVER['HTTP_ORIGIN'];
 
     header("Content-type: application/json; charset=UTF-8");
     header("Access-Control-Allow-Origin: *");
-=======
-
-    header("Content-type: application/json; charset=UTF-8");
-
-    $parts = explode('/', $_SERVER['REQUEST_URI']);
-    $argTime;
-
-    foreach($parts as $i){
-        if(strpos($i, "time") !== false){
-            $argTime = (float) filter_var($i, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-        }
-    }
->>>>>>> Mobile_app
 
     $method = $_SERVER['REQUEST_METHOD'];
     if ($method == "GET"){
@@ -24,13 +10,6 @@
         http_response_code(200);
         echo $output;
     }
-<<<<<<< HEAD
-=======
-    else if($method == "PUT"){
-        file_put_contents("../src/data/interval.txt", $argTime);
-        http_response_code(200);
-    }
->>>>>>> Mobile_app
     else{
         http_response_code(400);
     }
