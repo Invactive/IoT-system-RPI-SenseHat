b7c718f09edcb46bd8237673f623498b0abf5110
Jakub Grzesiak <gkuba8@gmail.com>
2023-04-27 14:43:24 +0200

    Update get_pressure.py

--- Server/api/src/python/get_pressure.py
+++ Server/api/src/python/get_pressure.py
@@ -2,10 +2,7 @@
 import json
 from sense_emu import SenseHat
 import time
-<<<<<<< HEAD
 import datetime
-=======
->>>>>>> Mobile_app
 
 
 sense = SenseHat()
@@ -14,12 +11,8 @@ while(True):
     press_hpa = sense.get_pressure() # millibars == hPa
     press_mmhg = press_hpa * 0.75006156130264
     result = {"press_hpa" : press_hpa, "press_mmhg": press_mmhg}
-<<<<<<< HEAD
     date = {"timestamp": str(datetime.datetime.now())}
     result.update(date)
-=======
-
->>>>>>> Mobile_app
     f = open("/home/jakub/Desktop/IoT-system-RPI-SenseHat/Server/api/src/data/pressure.dat", "w")
     f.write(json.dumps(result))
     f.close()
