a188805e1c46ce77b71efb28ebe947ec25b6f037
Jakub Grzesiak <gkuba8@gmail.com>
2023-04-27 14:44:16 +0200

    Update get_temperature.py

--- Server/api/src/python/get_temperature.py
+++ Server/api/src/python/get_temperature.py
@@ -2,10 +2,7 @@
 import json
 from sense_emu import SenseHat
 import time
-<<<<<<< HEAD
 import datetime
-=======
->>>>>>> Mobile_app
 
 
 sense = SenseHat()
@@ -23,12 +20,8 @@ while(True):
     result = {"temp": {"tempC":tempC, "tempF": tempF}, 
         "temp_humi": {"tempC": temp_humiC, "tempF": temp_humiF},
         "temp_press": {"tempC": temp_pressC, "tempF": temp_pressF}}
-<<<<<<< HEAD
     date = {"timestamp": str(datetime.datetime.now())}
     result.update(date)
-=======
-
->>>>>>> Mobile_app
     f = open("/home/jakub/Desktop/IoT-system-RPI-SenseHat/Server/api/src/data/temperature.dat", "w")
     f.write(json.dumps(result))
     f.close()
